eb86028efc6b5a635582440203e7511d61c81240
Y. Violet Guo <yueguo598@gmail.com>
2020-04-24 14:05:08 -0300

    rps cmmd line done

--- scripts/func.js
+++ scripts/func.js
@@ -1,6 +1,8 @@
 // constants
-
-let game_params =["rock", "paper","scissor"]
+const ROCK = "rock"
+const PAPER = "paper"
+const SCISSOR = "scissor"
+let game_params =[ROCK, PAPER, SCISSOR]
 
 
 function computerPlay(){
@@ -11,3 +13,61 @@ function computerPlay(){
     return game_params[choice];   
 }
 
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase()
+    let score_arr = [0, 0]; // player : computer scores
+
+    // one round of RPS
+    if(playerSelection == ROCK){
+        if(computerSelection == SCISSOR){
+            score_arr = [1, 0];
+        }
+        else if (computerSelection == PAPER){
+            score_arr = [0, 1];
+        }
+    }
+    else if(playerSelection == PAPER){
+        if(computerSelection == ROCK){
+            score_arr = [1, 0];
+        }
+        else if (computerSelection == SCISSOR){
+            score_arr = [0, 1];
+        }
+    }
+    else if (playerSelection == SCISSOR){
+        if(computerSelection == PAPER){
+            score_arr = [1, 0];
+        }
+        else if (computerSelection == ROCK){
+            score_arr = [0, 1];
+        }
+    }
+    return score_arr;
+}
+
+
+function game(){
+
+    let playerScore = 0;
+    let computerScore = 0;
+    for (let i = 0; i < 5; i++){
+        let playerMove = prompt("Your move");
+        let computerMove = computerPlay();
+
+        let score_one_round = playRound(playerMove, computerMove);
+        playerScore += score_one_round[0];
+        computerScore += score_one_round[1];
+        console.log(`Player ${playerScore} : computer ${computerScore}`);
+    }
+    if (playerScore > computerScore){
+        console.log("YOU WIN");
+    }
+    else if (playerScore < computerScore)
+        console.log("I WIN");
+    else 
+        console.log("TIE");
+
+}
+
+game();
